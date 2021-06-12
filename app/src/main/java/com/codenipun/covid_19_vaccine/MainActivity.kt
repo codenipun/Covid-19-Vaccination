2fa0be0d24863edbcd1213087d9697358e4751d5
nipunjain <nipunjain119@gmail.com>
2021-06-12 15:30:03 +0530

    Intial commit

---
+++ app/src/main/java/com/codenipun/covid_19_vaccine/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.codenipun.covid_19_vaccine
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener() {
+            intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
